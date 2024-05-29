fdf57e40e84aae1b5f028cea53b7704c82236d3c
Flávio Borba <fesb777poa@gmail.com>
2024-05-29 22:46:59 +0100

    correçoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,7 @@ window.onload = function(){
 };
 
 
-const imgs = document.querySelectorAll('.js-img');
-//console.log(imgs);
-const desc = document.querySelectorAll('.js-desc');
-//console.log(desc)
 
-function menuTab() {
-    //Serve para sempre manter o primeiro elemento visivel, já que o display é none e os elementos estao ocultados;
-    desc[0].classList.add('ativo');
-
-    //Essa função ativa um loop onde adiciona a classe 'ativo' no item clicado e remove das outras;
-    function activeTab(index) {
-        desc.forEach((item) => {
-            item.classList.remove('ativo')
-        });
-        desc[index].classList.add('ativo');
-
-    }
-    //essa função para adicionar o item a ser clicado e revelar o conteudo
-    //faz um loop no item pai, adiiconando eveneto de click para cada um, ativando a função ao clicar
-
-    imgs.forEach((title, index) => {
-        title.addEventListener('click', function () {
-            activeTab(index);
-        })
-    })
-}
-menuTab();
 
 //SCROLL ANIMADO
 
