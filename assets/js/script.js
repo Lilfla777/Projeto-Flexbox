664ce5eab5e268c21d25ec3d71219ab3e77484c5
Flávio Borba <fesb777poa@gmail.com>
2024-05-30 22:01:08 +0100

    ultimos destalhes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,3 @@
 
 
 
-//SCROLL ANIMADO
-
-const sections = document.querySelectorAll('.js-scroll');
-const metade = window.innerHeight * 0.7;
-function animaScroll() {
-    sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - metade < 0);
-        if (isSectionVisible) {
-            section.classList.add('ativarScroll');
-        } else {
-            section.classList.remove('ativarScroll')
-        }
-    })
-}
-window.addEventListener('scroll', animaScroll);
-animaScroll();
